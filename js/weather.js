3c77b0275d9ea5241b7aab1c88951f8942182525
vianao <1849792339@qq.com>
2022-12-18 02:36:14 +0800

    Site updated: 2022-12-18 02:36:13

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+WIDGET = {
+    "CONFIG": {
+      "modules": "01234",
+      "background": "5",
+      "tmpColor": "FF0000",
+      "tmpSize": "16",
+      "cityColor": "00FFFF",
+      "citySize": "16",
+      "aqiColor": "00FFFF",
+      "aqiSize": "16",
+      "weatherIconSize": "24",
+      "alertIconSize": "18",
+      "padding": "10px 10px 10px 10px",
+      "shadow": "0",
+      "language": "auto",
+      "fixed": "false",
+      "vertical": "top",
+      "horizontal": "left",
+      "key": "36ab0b0c06764a83970e275127ce7bfe"
+    }
+  }
