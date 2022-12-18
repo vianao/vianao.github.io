46df68091ec6f695488102789ad69580aaa19010
vianao <1849792339@qq.com>
2022-12-18 18:04:10 +0800

    Site updated: 2022-12-18 18:04:09

--- js/weather.js
+++ js/weather.js
@@ -1,21 +1,21 @@
 WIDGET = {
-    "CONFIG": {
-      "modules": "01234",
-      "background": "5",
-      "tmpColor": "FF0000",
-      "tmpSize": "16",
-      "cityColor": "00FFFF",
-      "citySize": "16",
-      "aqiColor": "00FFFF",
-      "aqiSize": "16",
-      "weatherIconSize": "24",
-      "alertIconSize": "18",
-      "padding": "10px 10px 10px 10px",
-      "shadow": "0",
-      "language": "auto",
-      "fixed": "false",
-      "vertical": "top",
-      "horizontal": "left",
-      "key": "36ab0b0c06764a83970e275127ce7bfe"
-    }
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "D9D9D9",
+    "tmpSize": "16",
+    "cityColor": "D9D9D9",
+    "citySize": "16",
+    "aqiColor": "D9D9D9",
+    "aqiSize": "16",
+    "weatherIconSize": "16",
+    "alertIconSize": "16",
+    "padding": "1px 1px 1px 1px",
+    "shadow": "0",
+    "language": "auto",
+    "fixed": "false",
+    "vertical": "top",
+    "horizontal": "left",
+    "key": "9a8039a2ab9642d7834650b3737ffb98"
   }
+}
