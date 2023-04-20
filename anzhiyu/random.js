6a00e90b9c1660dae08bee5fdd5bc9ee84b65ccd
vianao <1849792339@qq.com>
2023-04-20 11:32:03 +0800

    Site updated: 2023-04-20 11:32:01

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/c018.html","posts/undefined.html","posts/df2b.html","posts/74d3.html","posts/ee58.html","posts/37ae.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/c018.html","posts/undefined.html","posts/37ae.html","posts/ee58.html","posts/74d3.html","posts/df2b.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
